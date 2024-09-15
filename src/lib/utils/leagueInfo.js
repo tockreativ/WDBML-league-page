e3f4b2ea4c60550ae14ede543d7518a150b8e485
tocKreativ <5560387+tockreativ@users.noreply.github.com>
2024-09-15 17:32:37 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1048306723926216704"; // your league ID
 export const leagueName = "Win Lizzy Brandonisio's Money League"; // your league name
-export const dues = 30; // (optional) used in template constitution page
+export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
